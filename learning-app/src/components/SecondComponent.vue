ff924d215ebb9c66572d5653601b57d2f409cfe5
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-13 01:47:44 +0200

    binding event to button example and method example

--- learning-app/src/components/SecondComponent.vue
+++ learning-app/src/components/SecondComponent.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <h2>{{counter}}</h2>
+
+        <button v-on:click="IncrementCounter()">Dodaj 1 do countera</button>
+        <button v-on:click="DecrementCounter()">Odejmij 1 od countera</button>
     </div>
 </template>
 
@@ -9,7 +13,18 @@
 export default {
     data(){
         return{
-            title: 'Second title'
+            title: 'Second title',
+            counter: 0
+        }
+    },
+
+    methods:{
+        IncrementCounter(){
+            this.counter = this.counter + 1;
+        },
+
+        DecrementCounter(){
+            this.counter = this.counter - 1;
         }
     }
 }
